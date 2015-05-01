836e7ab01295b18af8f23b4b32fac428dc333041
Eric G. Wolfe <eric.wolfe@gmail.com>
2015-05-01 17:53:10 -0400

    Make octal mode explicit

--- recipes/default.rb
+++ recipes/default.rb
@@ -38,7 +38,7 @@ node['snmp']['groups']['v2c'].each_key { |key| groupnames << key }
 groupnames = groupnames.uniq
 
 template '/etc/snmp/snmpd.conf' do
-  mode 0600
+  mode 00600
   owner 'root'
   group 'root'
   variables(groups: groupnames)
